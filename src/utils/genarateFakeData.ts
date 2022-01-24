0b77052902a393c2448b8b2b41d1d7ba97f4a8d2
Mikael <dev.kov.mtl@gmail.com>
2022-01-24 10:42:29 -0500

    generate fake data and default and fill the state by default

---
+++ src/utils/genarateFakeData.ts
@@ -0,0 +1,47 @@
+import faker from '@faker-js/faker';
+
+const generateFakePersonalInfo = () => {
+  return {
+    randomLastName: faker.name.lastName(),
+    randomFirstName: faker.name.firstName(),
+    // randomPicture: faker.image.avatar(),
+    randomJobTitle: faker.name.jobTitle(),
+    randomAddress: faker.address.streetAddress(),
+    randomEmail: faker.internet.email(),
+    randomPhone: faker.phone.phoneNumber(),
+    randomDescription: faker.lorem.paragraph(),
+  };
+};
+
+const generateFakeWorkExperience = (from: string, until: string) => {
+  const [f, u] = faker.date.betweens(from, until);
+  return {
+    randomPosition: faker.name.jobDescriptor(),
+    randomEmployer: faker.company.companyName(),
+    // randomFrom: faker.date.
+    randomFrom: f.toLocaleDateString(),
+    randomUntil: u.toLocaleDateString(),
+    randomJobDescription: faker.lorem.paragraph(),
+  };
+};
+
+const generateFakeEducation = (from: string, until: string) => {
+  const [f, u] = faker.date.betweens(from, until);
+  return {
+    randomDegree: faker.lorem.word(),
+    randomSchoolName: faker.lorem.words(),
+    randomFrom: f.toLocaleDateString(),
+    randomUntil: u.toLocaleDateString(),
+  };
+};
+
+export const generateFakeResume = () => {
+  return {
+    personalInformation: generateFakePersonalInfo(),
+    workExperience: [
+      generateFakeWorkExperience('2013', '2015'),
+      generateFakeWorkExperience('2015', '2020'),
+    ],
+    education: [generateFakeEducation('1998', '2000')],
+  };
+};
